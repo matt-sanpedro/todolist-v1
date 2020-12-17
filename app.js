93521f7093b49e75aac068bde39b81e34f9a2b28
matt-sanpedro <mattsanpedro76@gmail.com>
2020-12-16 22:00:54 -0800

    add header color scheme per day of the week

--- app.js
+++ app.js
@@ -17,15 +17,17 @@ app.get('/', function(req, res){
   // getDay returns number 0-6 corresponding to the day of week
   var currentDay = today.getDay();
 
-  // day array
+  // day and color arrays for each day of the week [0-6]
   var dayArray = {0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday'};
+  var colorArray = {0: 'blue', 1: 'blue', 2: 'blue', 3: 'green', 4: 'green', 5: 'blue', 6: 'blue'};
 
-  var day = dayArray[currentDay]
+  var day = dayArray[currentDay];
+  var color = colorArray[currentDay];
 
   // render a file called list.ejs in views folder
-  res.render('list', {kindOfDay: day});
-})
+  res.render('list', {kindOfDay: day, headerColor: color});
+});
 
 app.listen(3000, function(){
   console.log('Server started on port 3000');
-})
+});
