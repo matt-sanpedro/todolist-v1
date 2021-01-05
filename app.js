62ce0fc2ad036902cd982c7601b328622dcf2169
matt-sanpedro <mattsanpedro76@gmail.com>
2021-01-04 22:22:56 -0800

    render list items using ejs template and item array

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser');
 
 // create app constant by using express
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 // tell app to use ejs
 app.set('view engine', 'ejs');
+
+// initialize variables
+var items = ['Eat', 'Sleep', 'Program', 'Coffee'];
 
 // create the get route
 app.get('/', function(req, res){
@@ -14,19 +18,32 @@ app.get('/', function(req, res){
   // console.log(today.getDay());
   // res.send('Hello');
 
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+
+  var day = today.toLocaleDateString('en-US', options);
+
+
   // getDay returns number 0-6 corresponding to the day of week
   var currentDay = today.getDay();
 
-  // day and color arrays for each day of the week [0-6]
-  var dayArray = {0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday'};
-  var colorArray = {0: 'blue', 1: 'blue', 2: 'blue', 3: 'green', 4: 'green', 5: 'blue', 6: 'blue'};
+  var colorArray = {0: 'blue', 1: 'green', 2: 'blue', 3: 'green', 4: 'green', 5: 'blue', 6: 'blue'};
 
-  var day = dayArray[currentDay];
   var color = colorArray[currentDay];
 
   // render a file called list.ejs in views folder
-  res.render('list', {kindOfDay: day, headerColor: color});
+  res.render('list', {kindOfDay: day, headerColor: color, newList: items});
 });
+
+app.post('/', function(req, res){
+  newItem = req.body.newItem;
+  items.push(newItem);
+  // redirect to home route and trigger the app.get
+  res.redirect('/');
+})
 
 app.listen(3000, function(){
   console.log('Server started on port 3000');
