ef3178897f7c0559c4126623e94a197c824788cc
matt-sanpedro <mattsanpedro76@gmail.com>
2021-01-06 21:59:59 -0800

    style with css, changed overall theme from purple/white to auburn red/white

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser');
 
 // create app constant by using express
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('public'));
 
 // tell app to use ejs
 app.set('view engine', 'ejs');
@@ -13,7 +17,7 @@ app.set('view engine', 'ejs');
 var items = ['Eat', 'Sleep', 'Program', 'Coffee'];
 
 // create the get route
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   var today = new Date();
   // console.log(today.getDay());
   // res.send('Hello');
@@ -30,21 +34,33 @@ app.get('/', function(req, res){
   // getDay returns number 0-6 corresponding to the day of week
   var currentDay = today.getDay();
 
-  var colorArray = {0: 'blue', 1: 'green', 2: 'blue', 3: 'green', 4: 'green', 5: 'blue', 6: 'blue'};
+  var colorArray = {
+    0: 'black',
+    1: 'black',
+    2: 'black',
+    3: 'black',
+    4: 'black',
+    5: 'black',
+    6: 'black'
+  };
 
   var color = colorArray[currentDay];
 
   // render a file called list.ejs in views folder
-  res.render('list', {kindOfDay: day, headerColor: color, newList: items});
+  res.render('list', {
+    kindOfDay: day,
+    headerColor: color,
+    newList: items
+  });
 });
 
-app.post('/', function(req, res){
+app.post('/', function(req, res) {
   newItem = req.body.newItem;
   items.push(newItem);
   // redirect to home route and trigger the app.get
   res.redirect('/');
 })
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log('Server started on port 3000');
 });
